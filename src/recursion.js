dd92aa47a4c7d45f6c8788f10479bf2c23716ab8
Aaron Deane <aaronmdeane@gmail.com>
2018-11-23 14:21:41 -0800

    updated work on Greatest Common Divisor

--- src/recursion.js
+++ src/recursion.js
@@ -205,6 +205,15 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+    if(x < 0 || y < 0) {
+        return null;
+    }else if(x === 0) {
+        return y;
+    }else if(y === 0) {
+        return x;
+    }
+        
+
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
